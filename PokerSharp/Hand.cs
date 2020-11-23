6fcab57da2f54267ee1f27da22326e7f5b71201b
Scott Doxey <neogeek0101@gmail.com>
2020-11-23 04:07:02 -0500

    Added Permutations and Shuffle methods to Hand class.

--- PokerSharp/Hand.cs
+++ PokerSharp/Hand.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Scott Doxey. All Rights Reserved. Licensed under the MIT License. See LICENSE in the project root for license information.
 
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PokerSharp
 {
@@ -19,6 +21,60 @@ namespace PokerSharp
         {
 
             AddRange(cards);
+
+        }
+
+        public IEnumerable<Hand> Permutations()
+        {
+
+            var results = new List<Hand>();
+
+            var numberOfPossibleCombinations = (int)Math.Pow(2, Count);
+
+            for (var i = 1; i < numberOfPossibleCombinations; i += 1)
+            {
+
+                var combination = new Hand();
+
+                for (var j = 0; j < Count; j += 1)
+                {
+
+                    if (((i >> j) & 1) == 1)
+                    {
+
+                        combination.Add(this[j]);
+
+                    }
+
+                }
+
+                results.Add(combination);
+
+            }
+
+            return results;
+
+        }
+
+        public Hand Shuffle(int seed)
+        {
+
+            var random = new Random(seed);
+
+            var shuffledList = new Hand(this.OrderBy(_ => random.Next()));
+
+            return shuffledList;
+
+        }
+
+        public Hand Shuffle()
+        {
+
+            var random = new Random();
+
+            var shuffledList = new Hand(this.OrderBy(_ => random.Next()));
+
+            return shuffledList;
 
         }
 
